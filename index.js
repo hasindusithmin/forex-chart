91454de6c713056e964eeca94a520b3818e2179c
hasindusithmin <hasindusithmin64@gmail.com>
2022-04-29 10:34:33 +0530

    responsize

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const chart = LightweightCharts.createChart(document.getElementById("chart"), {
+
+const opt_desktop = {
   width: 1000,
   height: 500,
   layout: {
@@ -24,38 +25,54 @@ const chart = LightweightCharts.createChart(document.getElementById("chart"), {
     timeVisible: true,
     secondsVisible: false,
   },
-});
+}
+const opt_mobile = {
+  width: 680,
+  height: 360,
+  layout: {
+    backgroundColor: "#000000",
+    textColor: "rgba(255, 255, 255, 0.9)",
+  },
+  grid: {
+    vertLines: {
+      color: "rgba(197, 203, 206, 0.5)",
+    },
+    horzLines: {
+      color: "rgba(197, 203, 206, 0.5)",
+    },
+  },
+  crosshair: {
+    mode: LightweightCharts.CrosshairMode.Normal,
+  },
+  priceScale: {
+    borderColor: "rgba(197, 203, 206, 0.8)",
+  },
+  timeScale: {
+    borderColor: "rgba(197, 203, 206, 0.8)",
+    timeVisible: true,
+    secondsVisible: false,
+  },
+}
 
-const candleSeries = chart.addCandlestickSeries({
+const opt2 = {
   upColor: "#00ff00",
   downColor: "#ff0000",
   borderDownColor: "rgba(255, 144, 0, 1)",
   borderUpColor: "rgba(255, 144, 0, 1)",
   wickDownColor: "rgba(255, 144, 0, 1)",
   wickUpColor: "rgba(255, 144, 0, 1)",
-});
+}
 
+
+const chart1 = LightweightCharts.createChart(document.getElementById("chart1"), opt_desktop);
+const chart2 = LightweightCharts.createChart(document.getElementById("chart2"), opt_mobile);
+const candleSeries1 = chart1.addCandlestickSeries(opt2);
+const candleSeries2 = chart2.addCandlestickSeries(opt2);
 fetch("https://forex.hasindusithmin.repl.co/data/eurusd=x/5m")
-  .then((res1) => res1.json())
-  .then((data1) => {
-    candleSeries.setData(data1);
-    fetch("https://sr.hasindusithmin.repl.co/sr/eurusd/5mins")
-      .then((res2) => res2.json())
-      .then((data2) => {
-        for( key in data2){
-            if (key !== 'name'){
-                candleSeries.createPriceLine({
-                price: data2[key],
-                color: "#be1238",
-                lineWidth: 2,
-                lineStyle: LightweightCharts.LineStyle.Solid,
-                axisLabelVisible: true,
-                title: `${key}`,
-                });
-            }
-        }
-        
-      });
+  .then((res) => res.json())
+  .then((data) => {
+    candleSeries1.setData(data);
+    candleSeries2.setData(data);
   });
 
 // const binanceSocket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@kline_15m");
